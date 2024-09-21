c9dde6cc9523f6127a2b5ec8da25af26ccac38d5
imavazq <ruterima@gmail.com>
2024-09-21 19:42:32 -0300

    Primer commit

---
+++ src/main/java/com/imavazq/public_business_api_rest/PublicBusinessApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.imavazq.public_business_api_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicBusinessApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicBusinessApiRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/imavazq/public_business_api_rest/PublicBusinessApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imavazq.public_business_api_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicBusinessApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
